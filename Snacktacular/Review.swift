9f8fa534b004bdafb6640c55e4b075887c579943
Alex Golden <goldenah@bc.edu>
2020-11-09 13:35:10 -0500

    8.15

--- Snacktacular/Review.swift
+++ Snacktacular/Review.swift
@@ -31,8 +31,19 @@ class Review {
     convenience init() {
         let reviewUserID = Auth.auth().currentUser?.uid ?? ""
         self.init(title: "", text: "", rating: 0, reviewUserID: reviewUserID, date: Date(), DocumentID: "")
-        
     }
+    convenience init(dictionary: [String: Any]) {
+        let title = dictionary["title"] as! String? ?? ""
+        let text = dictionary["text"] as! String? ?? ""
+        let rating = dictionary["rating"] as! Int ?? 0
+        let reviewUserID = dictionary["reviewUserID"] as! String? ?? ""
+        let documentID = dictionary["documentID"] as! String? ?? ""
+        let timeIntervalDate = dictionary["date"] as! TimeInterval? ?? TimeInterval()
+        let date = Date(timeIntervalSince1970: timeIntervalDate)
+        
+        self.init(title: title, text: text, rating: rating, reviewUserID: reviewUserID, date: date, DocumentID: documentID)
+    }
+    
     func saveData(spot: Spot, completion: @escaping (Bool) -> ()) {
            let db = Firestore.firestore()
            // create the dictionary representing data we want to save
--- Snacktacular/Reviews.swift
+++ Snacktacular/Reviews.swift
@@ -4,16 +4,32 @@
 //
 //  Created by Alex Golden on 11/9/20.
 //
-
-import Foundation
 import Firebase
-
+import Foundation
 
 class Reviews {
     var reviewArray: [Review] = []
     var db: Firestore!
     init() {
         db = Firestore.firestore()
+    }
+    func loadData(spot: Spot, completed: @escaping () -> ()) {
+        guard spot.documentID != "" else {
+            return
+        }
+        db.collection("spots").document(spot.documentID).collection("reviews").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("Error adding the snapshot listener \(error!.localizedDescription)")
+                return completed()
+            }
+            self.reviewArray = []
+            for document in querySnapshot!.documents {
+                let review = Review(dictionary: document.data())
+                review.documentID = document.documentID
+                self.reviewArray.append(review)
+            }
+            completed()
+        }
     }
 }
 
